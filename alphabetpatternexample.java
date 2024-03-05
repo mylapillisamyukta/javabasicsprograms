b67690996afb1909260617ad79a9e055817c44ce
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-05 10:51:27 +0530

    Add files via upload

---
+++ alphabetpatternexample.java
@@ -0,0 +1,36 @@
+package alphabetpattern;
+
+public class alphabetpatternexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/* 1.	char ch='A';
+    for(int i=1;i<=5;i++) 
+    {
+    	ch='A';
+	for(int j=1;j<=i;j++) {
+		System.out.print(ch+" ");
+	ch++;
+    }
+	System.out.println(" "); --output  A
+	                                   A B
+	                                   A B C
+	                                   A B C D
+	                                   A B C D E */
+	    for(int i=1;i<=5;i++) 
+	    {
+	    char ch='A';
+		for(int j=1;j<=i;j++) {
+			System.out.print(ch+" ");
+		ch++;
+	    }
+		System.out.println(" ");
+		}
+	}
+}
+	
+
+
+		
+
+
