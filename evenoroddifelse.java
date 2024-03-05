3fe7abb291f6881521195841179d87ac1284fc09
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-05 10:56:37 +0530

    Add files via upload

---
+++ evenoroddifelse.java
@@ -0,0 +1,17 @@
+package evenorodd;
+
+public class evenoroddifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=14;
+		if(number %2 ==0) {
+			System.out.println(" no. is an even number");
+		}
+		else {
+			System.out.println(" no. is an odd number");
+		}
+
+	}
+
+}
