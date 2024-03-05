49d85628bc2320fdbbb8f982a487f0c8e85cf4fe
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-05 11:03:15 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,13 @@
+package ternaryoperator;
+
+public class ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=13;
+		String output=(number%2==0)?"even number":"odd number";
+		System.out.println(output);
+
+	}
+
+}
