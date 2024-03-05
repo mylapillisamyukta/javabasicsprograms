02fecc169b94d789e72830f2c17f54bedc7889d5
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-05 11:01:37 +0530

    Add files via upload

---
+++ reversepyramidexample.java
@@ -0,0 +1,17 @@
+package reversepyramid;
+
+public class reversepyramidexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int term =6;
+		for(int i=1;i<=term;i++) {
+			for(int j=term;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
