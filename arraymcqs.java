3393678231de73c782462afbd102dbd609a2a949
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-09 21:49:13 +0530

    Add files via upload

---
+++ arraymcqs.java
@@ -0,0 +1,38 @@
+package arrayassessment;
+
+public class arraymcqs {
+
+	public static void main(String[] args) {
+/*  11.	int arr[] = {10, 20, 30, 40, 50};
+		for(int i=0; i < arr.length; i++)
+		{
+		System.out.print(" " + arr[i]);
+		}   ------>10 20 30 40 50  */
+		
+/* 	12.int arr[] = new int [5];
+		System.out.print(arr); ----->hashcode in hexadecimalform [I@27f674d */
+		
+		
+/*	13.	int array_variable [] = new int[10];
+		for (int i = 0; i < 10; ++i)
+		{
+		array_variable[i] = i;
+		System.out.print(array_variable[i] + " ");
+		i++;   ----> 0 2 4 6 8    */
+	    
+/*  14.	int arr[] = new int[] {0 , 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int n = 6;
+		n = arr[arr[n] / 2];
+		System.out.print(n);   ---->3   */
+		
+	char array_variable [] = new char[10];
+		for (int i = 0; i < 10; ++i)
+		{
+		array_variable[i] = 'i';
+		System.out.print(array_variable[i] + " ");
+		}
+
+	}
+	}
+	
+
