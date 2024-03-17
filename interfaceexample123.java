c22651feec8fdbb07ea15c1565fb83fbe903cb85
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-17 20:57:45 +0530

    Add files via upload

---
+++ interfaceexample123.java
@@ -0,0 +1,37 @@
+package interfaceexample;
+interface hero123
+{
+	String x = "abc";
+	void show();
+	int y = 10;
+
+}
+interface hero345
+{
+	void show();
+	void print();
+	int y = 20;
+}
+class heroine123 implements hero123,hero345
+{
+public void show()
+	{
+	    System.out.println("i am inside class hero123");	
+	}
+public void print()
+{
+	System.out.println("i am inside class hero345");
+	System.out.println(hero123.y + hero345.y);
+}
+
+}
+
+public class interfaceexample123 {
+
+	public static void main(String[] args) {
+		heroine123 ob1 = new heroine123();
+		ob1.show();
+		ob1.print();
+	}
+}
+
