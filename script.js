8dddf0eec17f306f6a8e2ca6341033c164cf30d5
mylapillisamyukta <162086795+mylapillisamyukta@users.noreply.github.com>
2024-03-24 22:10:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var x="samyukta";
+var y="123";
+document.write(x);
+document.write(y);
+alert("hello");
+let p="welcome to java";
+let q="456";
+function m1()
+{
+    alert(p);
+}
+function m2()
+{
+    alert(q);
+}
+m1();
+m2();
+document.write(p);
+document.write(q);
+alert("java");
